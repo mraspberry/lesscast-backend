4dbcdbda5aaff900c430bc3982f55c17a771e617
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2021-09-15 20:31:37 -0500

    WIP: Fixed issues and added prettier

--- lib/lesscast-backend-stack.ts
+++ lib/lesscast-backend-stack.ts
@@ -1,45 +1,46 @@
-import * as cdk from '@aws-cdk/core';
-import * as ecs from '@aws-cdk/aws-ecs';
-import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as s3n from '@aws-cdk/aws-s3-notifications';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3n from "@aws-cdk/aws-s3-notifications";
+import * as sqs from "@aws-cdk/aws-sqs";
 
 export class LesscastBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Basic objects
-    const queue: sqs.Queue = new sqs.Queue(this, 's3_queue', {
+    const queue: sqs.Queue = new sqs.Queue(this, "s3_queue", {
       retentionPeriod: cdk.Duration.days(1),
       visibilityTimeout: cdk.Duration.hours(1),
     });
-    const webBucket: s3.Bucket = new s3.Bucket(this, 'web', {
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: "index.html"
-    });
-    const mediaBucket: s3.Bucket = new s3.Bucket(this, 'media', {
+    const mediaBucket: s3.Bucket = new s3.Bucket(this, "media", {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const image: ecs.ContainerImage = ecs.ContainerImage.fromAsset('transcoder');
-    const scalingSteps = [
-      {change: -1, upper: 0},
-      {change: 1, lower: 0},
-    ];
-    const command = [mediaBucket.bucketName, queue.queueUrl];
-    const ecsServiceConfig = { image, command, scalingSteps, queue, cpu: 4096 };
-    const ecsService = new ecsPatterns.QueueProcessingFargateService(
-      this, "transcoder_service", ecsServiceConfig,
+    const containerImage: ecs.ContainerImage = ecs.ContainerImage.fromRegistry(
+      "ghcr.io/mraspberry/lesscast-transcoder:0.6"
     );
-
+    const ecsService = new ecsPatterns.QueueProcessingFargateService(
+      this,
+      "transcoder_service",
+      {
+        image: containerImage,
+        scalingSteps: [
+          { change: -1, upper: 0 },
+          { change: 1, lower: 1 },
+        ],
+        queue: queue,
+        cpu: 512,
+        memoryLimitMiB: 2048,
+      }
+    );
+    
     // Permission grants
     mediaBucket.addObjectCreatedNotification(new s3n.SqsDestination(queue), {
-      prefix: 'video/'
+      prefix: "video/",
     });
     mediaBucket.grantReadWrite(ecsService.taskDefinition.taskRole);
-    
   }
 }
--- test/lesscast-backend.test.ts
+++ test/lesscast-backend.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as LesscastBackend from '../lib/lesscast-backend-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as LesscastBackend from "../lib/lesscast-backend-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new LesscastBackend.LesscastBackendStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new LesscastBackend.LesscastBackendStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
